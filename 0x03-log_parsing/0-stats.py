8995bc30ce5b1114db21aad2d01fd74feb364095
Katekani Madingana <madinganakn@gmail.com>
2023-12-23 18:36:59 +0200

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,37 @@
 #!/usr/bin/python3
-""" Log Parsing Module """
+'''a script that reads stdin line by line and computes metrics'''
+
+
 import sys
 
-
-STATUS = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-n = 0
+counter = 0
+
 try:
-    for argument in sys.stdin:
-        arguments = argument.split(" ")
-        if len(arguments) > 2:
-            status = arguments[-2]
-            file_size = int(arguments[-1])
-            if status in STATUS:
-                STATUS[status] += 1
-            total_size += file_size
-            n += 1
-            if n == 10:
-                print("File size: {:d}".format(total_size))
-                for key, value in sorted(STATUS.items()):
-                    if value != 0:
-                        print("{}: {:d}".format(key, value))
-                n = 0
-except KeyboardInterrupt:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
+
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
     pass
+
 finally:
-    print("File size: {:d}".format(total_size))
-    for key, value in sorted(STATUS.items()):
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
         if value != 0:
-            print("{}: {:d}".format(key, value))
+            print('{}: {}'.format(key, value))
